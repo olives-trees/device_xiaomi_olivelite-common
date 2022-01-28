518485e10ac261c9c36120298335096f4adaa548
JonnyRoller23 <maxim55miui11@gmail.com>
2022-01-28 16:03:53 +0000

    sdm439-common: Removed support for long screenshot

--- sdm439.mk
+++ sdm439.mk
@@ -98,12 +98,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
-# LongShot
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/com.asus.stitchimage.service-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/com.asus.stitchimage.service-hiddenapi-package-whitelist.xml \
-    $(LOCAL_PATH)/configs/com.asus.stitchimage-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/com.asus.stitchimage-hiddenapi-package-whitelist.xml      
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml     
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
@@ -134,15 +129,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     GoogleRecorder                       
-
-PRODUCT_PACKAGES += \
-    CustomQSLonghsot    
-
-PRODUCT_PACKAGES += \
-    StitchImage
-
-PRODUCT_PACKAGES += \
-    StitchImageService
 
 PRODUCT_PACKAGES += \
     libwa_dof
@@ -366,11 +352,7 @@ PRODUCT_PACKAGES += \
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/privapp-permissions-huaqin.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-huaqin.xml \
-    $(LOCAL_PATH)/permissions/privapp-permissions-com.asus.stitchimage.service.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.asus.stitchimage.service.xml \
-    $(LOCAL_PATH)/permissions/privapp-permissions-com.asus.stitchimage.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.asus.stitchimage.xml \
-    $(LOCAL_PATH)/permissions/privapp-permissions-com.customqs.lonshot.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.customqs.lonshot.xml \
-    $(LOCAL_PATH)/default-permissions/default-asus-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/default-asus-permissions.xml
+    $(LOCAL_PATH)/permissions/privapp-permissions-huaqin.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-huaqin.xml
 
 # Google Photos
 PRODUCT_COPY_FILES += \
